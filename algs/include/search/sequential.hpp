b3f1346c13d933369ad4963ad81131daa2f5bb8f
Syaoming <syaoming13@gmail.com>
2017-05-08 15:25:07 +0800

    two new file

---
+++ algs/include/search/sequential.hpp
@@ -0,0 +1,15 @@
+#ifndef ALGS_SEARCH_SEQUENTIAL_HPP
+#define ALGS_SEARCH_SEQUENTIAL_HPP
+
+/*
+ * sequantial.hpp
+ * Sequantial Search Symbol Table,
+ */
+
+namespace algs {
+
+
+
+} // namespace algs
+
+#endif
