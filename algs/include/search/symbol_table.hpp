b97c846e8c0f48de783b02d181418a588f22512f
Syaoming <syaoming13@gmail.com>
2017-05-08 18:41:22 +0800

    structure comments

--- algs/include/search/symbol_table.hpp
+++ algs/include/search/symbol_table.hpp
@@ -31,12 +31,12 @@ public:
   //iterator cend(Key k) = 0; // at least one past k
 
   // element access and modifiers
-  virtual void put(const Key& k, const Value& v) = 0;  // put a key-value pair
-  virtual void put(Key&& k, Value&& v) = 0;  // move a key-value pair
-  virtual const Value& get(const Key& k) const = 0;          // get a value by key
-  virtual Value pop(const Key& k) = 0; // erase a key-value pair
-  virtual Value pop_min() { return pop(min()); }  // erase smallest key
-  virtual Value pop_max() { return pop(max()); }  // erase largest key
+  virtual void put(const Key& k, const Value& v) = 0; // put a key-value pair
+  virtual void put(Key&& k, Value&& v) = 0;           // move a key-value pair
+  virtual const Value& get(const Key& k) const = 0; // get a value by key
+  virtual Value pop(const Key& k) = 0;           // erase a key-value pair
+  virtual Value pop_min() { return pop(min()); } // erase smallest key
+  virtual Value pop_max() { return pop(max()); } // erase largest key
 
   // capacity
   virtual bool empty() const = 0; // is the table empty ?
@@ -46,11 +46,12 @@ public:
   virtual bool contains(const Key& k) const = 0; // existency query
   virtual const Key& min() const = 0; // smallest key
   virtual const Key& max() const = 0; // greatest key
-  virtual const Key& floor(const Key& k) const = 0;   // largest key that is <= k
-  virtual const Key& ceiling(const Key& k) const = 0; // smallest key that is >= k
-  virtual size_type rank(const Key& k) const = 0;    // number of keys < k
+  virtual const Key& floor(const Key& k) const = 0;   // largest key <= k
+  virtual const Key& ceiling(const Key& k) const = 0; // smallest key >= k
+  virtual size_type rank(const Key& k) const = 0; // number of keys < k
   virtual const Key& select(size_type r) const = 0; // key of rank r
-  virtual size_type size(const Key& lo, const Key& hi) const = 0; // number of keys in [lo, hi]
+  virtual size_type size(const Key& lo, const Key& hi) const = 0; // number of
+                                                           // keys in [lo, hi]
 
 };
 
