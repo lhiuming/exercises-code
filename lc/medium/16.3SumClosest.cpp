f5c8aa49e06dd34c9258cc7f31b7758cc324c2c3
Syaoming <syaoming13@gmail.com>
2017-05-12 17:24:05 +0800

    copy typo

--- lc/medium/16.3SumClosest.cpp
+++ lc/medium/16.3SumClosest.cpp
@@ -7,13 +7,6 @@
  *
  * https://leetcode.com/problems/3sum-closest/#/description
  */
- /*
-  * "
-  * Given an array S of n integers, find three integers in S such that the sum
-  * is closest to a given number, target. Return the sum of the three integers.
-  * You may assume that each input would have exactly one solution.
-  * "
-  */
 
 #include <cstddef>
 #include <vector>
