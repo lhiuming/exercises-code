a85a947d8ce8ba66dc78efa13072dfc5991c153b
Syaoming <syaoming13@gmail.com>
2017-05-24 11:35:43 +0800

    add full stl interface

--- algs/include/fundamentals/stack.hpp
+++ algs/include/fundamentals/stack.hpp
@@ -5,8 +5,9 @@
 
 /*
  * stack.hpp
- * LIFO Stack implemented with linked list.
- * (Very simple, because List<> has done everything.)
+ * LIFO Stack implemented with linked list. This class use the same interface
+ * as std::stack, except that it doesn't support customized container (it is
+ * not a container adapter)
  *
  * TODO: add full STL compatibility
  */
@@ -15,12 +16,17 @@ namespace algs {
 
 template<class Item>
 class Stack {
-
 public:
 
-  using size_type = typename List<Item>::size_type;
+  // Member types
+  using container_type  = List<Item>;
+  using value_type      = typename container_type::value_type;
+  using size_type       = typename container_type::size_type;
+  using reference	      = typename container_type::reference;
+  using const_reference	= typename container_type::const_reference;
 
   // Default constructor
+  // TODO: support complete construtor family
   Stack() {};
 
   // Copy Constrol : basically controlled by List<Item>
@@ -28,12 +34,30 @@ public:
   Stack(const Stack&) = default;
   Stack& operator=(const Stack&) = default;
 
-  // basic interface
+  // Element access
+  reference top() { return data.pop_front(); }
+  const_reference top() const { return top(); }
+
+  // Capacity
+  bool empty() const { return data.empty(); }
+  size_type size() const { return N; }
+
+  // Modifiers //
+
   void push(const Item& item) { data.push_front(item); ++N; }
-  void push(Item&& item) { data.push_front(item); ++N; }
-  Item pop() { return data.pop_front(); --N; }
-  bool empty() { return data.empty(); }
-  size_type size() { return N; }
+  void push(Item&& item) { data.push_front(std::move(item)); ++N; }
+
+  template< class... Args > // using c++ 17 version
+  reference emplace( Args&&... args ) {
+    return data.emplace_front(std::forward<Args>(args)...); }
+
+  // NOTE: std::stack version return void
+  Item pop() { --N; return data.pop_front(); }
+  void swap(Stack& other) noexcept {
+    using std::swap;
+    swap(data, other.data);
+  }
+
 
 private:
 
@@ -41,6 +65,25 @@ private:
   size_type N = 0;
 
 };
+
+
+// Non-member functions //
+// TODO
+
+// operator==
+// operator!=
+// operator<
+// operator<=
+// operator>
+// operator>=
+// std::swap(algs::Stack) (specification in std namespace)
+
+
+// Helper class //
+// TODO
+
+// std::uses_allocator<std::stack>
+
 
 } // namespace algs
 
