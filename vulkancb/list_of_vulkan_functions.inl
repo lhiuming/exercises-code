5e52604e3b342752dc1245ffc7d620cde98baddc
syaoming <syaoming13@gmail.com>
2018-07-08 22:48:15 +0800

    add root gitignores; add some vulkancb c1 stuffs

---
+++ vulkancb/list_of_vulkan_functions.inl
@@ -0,0 +1,34 @@
+/*
+ * A snippet of codes to define all vaulkan functions.
+ * Related preprocessor should be deinfe before including this file.
+ */
+
+//
+  
+#ifndef EXPORTED_VULKAN_FUNCTION( function )
+#define EXPORTED_VULKAN_FUNCTION( function )
+#endif
+
+EXPORTED_VULKAN_FUNCTION(vkGetInstanceProcAddr)
+
+#undef EXPORTED_VULKAN_FUNCTION( function )
+
+//
+
+#undef GLOBAL_LEVEL_VULKAN_FUNCTION( function )
+
+//
+
+#undef INSTANCE_LEVEL_VULKAN_FUNCTION( function )
+
+//
+
+#undef INSTANCE_LEVEL_VULKAN_FUNCTION_FROM_EXTENSION( function )
+
+//
+
+#undef DEVICE_LEVEL_VULKAN_FUNCTION( function )
+
+//
+
+#undef DEVICE_LEVEL_VULKAN_FUNCTION_FROM_EXTENSION( function )
---
+++ vulkancb/vulkan_functions.h
@@ -0,0 +1,25 @@
+#ifndef VULKAN_FUNCTIONS_H
+#define VULKAN_FUNCTIONS_H
+
+#define VK_NO_PROTOTYPE // disable the decls for statically-linked vulkan lib.
+#include "external/vulkan.h"
+
+namespace vulkan {
+
+#define EXPORTED_VULKAN_FUNCTION( name ) extern PFN_##name name;
+
+#define GLOBAL_LEVEL_VULKAN_FUNCTION( name ) extern PFN_##name name;
+
+#define INSTANCE_LEVEL_VULKAN_FUNCTION( function ) extern PFN_##name name;
+
+#define INSTANCE_LEVEL_VULKAN_FUNCTION_FROM_EXTENSION( function ) extern PFN_##name name;
+
+#define DEVICE_LEVEL_VULKAN_FUNCTION( function ) extern PFN_##name name;)
+
+#define DEVICE_LEVEL_VULKAN_FUNCTION_FROM_EXTENSION( function ) extern PFN_##name name;
+
+#include "list_of_vulkan_functions.inl"
+  
+}
+
+#endif // VULKAN_FUNCTIONS_H
